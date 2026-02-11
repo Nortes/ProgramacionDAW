0a1e278a7882a7277c14c04027ba3af2ed8f04b7
miken <mikenorts@gmail.com>
2026-02-11 08:05:26 +0100

    Encapsulación

---
+++ Colecciones/src/Contacto.java
@@ -0,0 +1,27 @@
+public class Contacto {
+    private final String name;
+    private final String phoneNumber;
+
+    Contacto(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public static void createContact(String name, String phoneNumber) {
+        new Contacto(name, phoneNumber);
+    }
+
+   @Override
+    public boolean equals(Object o) {
+       return this.name.equals(((Contacto) o).getName()) || this.phoneNumber.equals(((Contacto) o).getPhoneNumber());
+   }
+
+}
---
+++ Colecciones/src/TelefonoMovil.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+
+public class TelefonoMovil {
+    private final String myNumber;
+    private final ArrayList<Contacto> myContacts;
+
+    public TelefonoMovil(String number) {
+        myNumber = number;
+        myContacts = new ArrayList<>();
+    }
+
+    public boolean addNewContact (Contacto numero){
+        if(findContact(numero)>=0){
+            return false;
+        }
+
+        myContacts.add(numero);
+        return true;
+    }
+
+    public boolean updateContact (Contacto antiguo,Contacto nuevo){
+        if(findContact(antiguo)>=0){
+            return false;
+        }else {
+            if (myContacts.contains(antiguo)) {
+                myContacts.set(myContacts.indexOf(antiguo), nuevo);
+            }
+            return true;
+        }
+    }
+
+    public boolean removeContact (Contacto numero){
+           if (findContact(numero) >= 0) {
+               myContacts.remove(numero);
+               return true;
+           }
+
+        return false;
+    }
+
+    private int findContact (Contacto numero){
+        return myContacts.indexOf(numero);
+    }
+
+    private int findContact (String nombre){
+        for(int i=0;i<myContacts.size();i++){
+            if(myContacts.get(i).getName().equals(nombre)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public Contacto queryContact (String nombre){
+        int index=findContact(nombre);
+        if(index>=0){
+          return myContacts.get(index);
+        }
+        else return null;
+    }
+
+    public void printContacts(){
+        if(myContacts.isEmpty()){
+            System.out.println("No tienes contactos");
+        }
+        else {
+            System.out.println("Lista de Contactos:");
+            for (Contacto myContact : myContacts) {
+                System.out.println("1. " + myContact.getName() + " → " + myContact.getPhoneNumber());
+            }
+        }
+    }
+
+    public String getMyNumber() {
+        return myNumber;
+    }
+}
